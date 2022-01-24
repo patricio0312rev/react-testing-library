e4f5d6fb844bfba737836cc5b585da662c6357b6
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-24 09:32:04 -0500

    Test para verificar que el texto se renderice en pantalla

--- src/Hello.test.jsx
+++ src/Hello.test.jsx
@@ -1,8 +1,10 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import Hello from './Hello.component';
+import { render, screen } from '@testing-library/react';
 
 test("Component should display Hello text", () => {
     const view = render(<Hello />);
-
+    // view.debug();
+    let helloText = view.getByText("Hello");
+    expect(helloText).toBeTruthy();
 });
