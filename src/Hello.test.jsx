38a1c340801006a8ae1c3490e8e6317aad0454d3
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-24 09:36:51 -0500

    Descomponiendo metodos de elemento

--- src/Hello.test.jsx
+++ src/Hello.test.jsx
@@ -3,7 +3,7 @@ import Hello from './Hello.component';
 import { render, screen } from '@testing-library/react';
 
 test("Component should display Hello text", () => {
-    const view = render(<Hello />);
+    /* const view = render(<Hello />);
     // view.debug();
     let helloText = view.getByText("Hello");
     expect(helloText).toBeTruthy();
@@ -12,5 +12,11 @@ test("Component should display Hello text", () => {
     expect(helloText.tagName).toBe('H1');
     
     console.log(helloText.textContent);
-    expect(helloText.textContent).toBe('Hello');
+    expect(helloText.textContent).toBe('Hello'); */
+
+    
+    const { getByText, queryAllByText, debug } = render(<Hello />);
+    debug();
+    let helloText = getByText("Hello");
+    expect(helloText).toBeTruthy();
 });
